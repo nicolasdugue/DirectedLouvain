56c367573a9aa1d8fc7f0a3bc701d92eb90ee4b2
anthony <anthony.perez@uinv-orleans.fr>
2022-02-18 14:17:18 +0100

    example file for python binding

---
+++ src/pybind_main.cpp
@@ -0,0 +1,14 @@
+#include "../include/community.hpp"
+#include "../extern/pybind11/include/pybind11/pybind11.h"
+#include "../extern/pybind11/include/pybind11/operators.h"
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(directedlouvain, dl){
+	py::class_<Community>(dl, "init")
+		.def(py::init<const string &, bool, const double, bool, bool>(), 
+                        py::arg("filename"), py::arg("weighted")=false, py::arg("precision")=0.0001, py::arg("reproducibility")=false, py::arg("renumbering")=true)
+		.def("run", &Community::run, 
+                        py::arg("verbose")=false, py::arg("display_level")=-1, py::arg("filename_part")="")
+		.def("modularity",&Community::modularity);
+}
