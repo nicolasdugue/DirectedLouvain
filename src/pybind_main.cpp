0251793fce78f6047744d121c7e699b9e50ae230
anthony <anthony.perez@uinv-orleans.fr>
2022-02-25 08:49:34 +0100

    allowing std::cout redirection

--- src/pybind_main.cpp
+++ src/pybind_main.cpp
@@ -5,14 +5,17 @@
 #include "../include/community.hpp"
 #include "../extern/pybind11/include/pybind11/pybind11.h"
 #include "../extern/pybind11/include/pybind11/operators.h"
+#include "../extern/pybind11/include/pybind11/iostream.h"
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(directedlouvain, dl){
-	py::class_<Community>(dl, "init")
+	py::class_<Community>(dl, "Community")
 		.def(py::init<const string &, bool, const double, bool, bool>(), 
                         py::arg("filename"), py::arg("weighted")=false, py::arg("precision")=0.0001, py::arg("reproducibility")=false, py::arg("renumbering")=true)
 		.def("run", &Community::run, 
-                        py::arg("verbose")=false, py::arg("display_level")=-1, py::arg("filename_part")="")
+                        py::arg("verbose")=false, py::arg("display_level")=-1, py::arg("filename_part")="", 
+                        py::call_guard<py::scoped_ostream_redirect,
+                        py::scoped_estream_redirect>())
 		.def("modularity",&Community::modularity);
 }
