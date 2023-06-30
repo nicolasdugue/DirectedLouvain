688b3cab9f9740654f83aeb6828e4a44a3d79b07
anthonimes <anthony.perez@univ-orleans.fr>
2023-06-30 10:15:16 +0200

    Update pybind_main.cpp
    Adding resolution parameter and a function that returns the hierarchy structure

--- src/pybind_main.cpp
+++ src/pybind_main.cpp
@@ -6,20 +6,25 @@
 #include "../extern/pybind11/include/pybind11/pybind11.h"
 #include "../extern/pybind11/include/pybind11/operators.h"
 #include "../extern/pybind11/include/pybind11/iostream.h"
+#include "../extern/pybind11/include/pybind11/stl.h"
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(directedlouvain, dl){
-	py::class_<Community>(dl, "Community")
-		.def(py::init<const string &, bool, const double, bool, bool>(), 
-                        py::arg("filename"), py::arg("weighted")=false, py::arg("precision")=0.0001, py::arg("reproducibility")=false, py::arg("renumbering")=true)
-		.def("run", &Community::run, 
-                        py::arg("verbose")=false, py::arg("display_level")=-1, py::arg("filename_part")="", 
-                        py::call_guard<py::scoped_ostream_redirect,
-                        py::scoped_estream_redirect>())
-		.def("modularity",&Community::modularity)
-                .def("print_level",&Community::print_level,
-                        py::arg("level")=0,
-                        py::call_guard<py::scoped_ostream_redirect,
-                        py::scoped_estream_redirect>());
+PYBIND11_MODULE(directedlouvain, dl) {
+    py::class_<Community>(dl, "Community")
+            .def(py::init<const string &, bool, const double, const double, bool, bool>(),
+                 py::arg("filename"), py::arg("weighted") = false, py::arg("precision") = 0.0001, py::arg("gamma") = 1,
+                 py::arg("reproducibility") = false, py::arg("renumbering") = true)
+            .def("run", &Community::run,
+                 py::arg("verbose") = false, py::arg("display_level") = -1, py::arg("filename_part") = "",
+                 py::call_guard<py::scoped_ostream_redirect,
+                 py::scoped_estream_redirect>())
+            .def("modularity", &Community::modularity)
+            .def("last_level", &Community::get_last_level)
+            .def("get_level", &Community::get_level,
+                 py::arg("level") = 0)
+            .def("print_level", &Community::print_level,
+                 py::arg("level") = 0,
+                 py::call_guard<py::scoped_ostream_redirect,
+                 py::scoped_estream_redirect>());
 }
